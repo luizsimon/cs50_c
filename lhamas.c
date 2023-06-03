f7a8905b848e64fab8805f5ca9ae73e6129107da
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-03 19:25:59 +0000

    Sat, Jun 3, 2023, 7:25 PM +00:00

--- lhamas.c
+++ lhamas.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-#include <cs50.i>
+#include <cs50.h>
 
 int main(void)
 {
-    //int i = 100;
-    int f = 200;
-    int y = 0;
-    for (int i = 100; i < f; i = i + i/3 - i/4; y++)
-  printf("Aninhos: %i\n", y);
+  do
+  {
+    int populacao_inicial = get_int("Qual o tamanho da população inicial? ");
+  }
+    while (populacao_inicial < 9)
 }
 
 
+
+
+
+
+
+ //int i = 100;
+   // int f = 200;
+  //  int y = 0;
+  //  for (int i = 100; i < f; i = i + i/3 - i/4; y++)
+//  printf("Aninhos: %i\n", y);
