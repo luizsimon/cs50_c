eaac4ca4d7fdec35928f4f9199f1fb4bcbf22306
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-03 19:10:09 +0000

    Sat, Jun 3, 2023, 7:10 PM +00:00

--- lhamas.c
+++ lhamas.c
@@ -2,13 +2,17 @@
 
 int main(void)
 {
-    int i = 100;
+    //int i = 100;
     int f = 200;
     int y = 0;
-    while (i < f)
-  {
-    i = i + i/3 - i/4;
-    y++;
-  }
+    for (int i = 100; i < f; i = i + i/3 - i/4)
+    {
+       y++;
+    }
   printf("Aninhos: %i\n", y);
 }
+ //  while (i < f)
+//  {
+  //  i = i + i/3 - i/4;
+  //  y++;
+//  }
