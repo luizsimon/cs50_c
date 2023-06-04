afab7ad71b0e6c382f315d0d5cf9345972a34da5
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-04 21:35:09 +0000

    Sun, Jun 4, 2023, 6:35 PM -03:00

--- population.c
+++ population.c
@@ -9,8 +9,8 @@ int main(void)
 {
     int pop_i = populacao_inicial();
     int pop_f = populacao_final(pop_i);
-    int anos = calculo(pop_i, pop_f);
-    printf("Years: %i", anos);
+    int years = calculo(pop_i, pop_f);
+    printf("Years: %i", years);
 }
 
 int populacao_inicial(void)
@@ -37,11 +37,11 @@ int populacao_final(int pop_i)
 
 int calculo(int pop_i, int pop_f)
 {
-    int anos = 0;
+    int years = 0;
     while (pop_i < pop_f)
     {
         pop_i = pop_i + pop_i / 3 - pop_i / 4;
-        anos++;
+        years++;
     }
-    return anos;
+    return years;
 }
