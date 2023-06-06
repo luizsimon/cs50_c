34368d727adc17055c71f854510a50e097132819
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-06 19:21:13 +0000

    Tue, Jun 6, 2023, 4:21 PM -03:00

--- prime.c
+++ prime.c
@@ -39,6 +39,8 @@ void IsPrime(int initial_number, int final_number)
 
         }
         while (j > i);
+
+        printf("//");
     }
 
     // int prime = (i % 2) + (i % 3) + (i % 5) + (i % 7) + (i % 9);
