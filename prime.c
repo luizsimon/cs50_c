63047c8d6b8921bf8c6a7164118b98706f3d4c4c
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-06 18:47:10 +0000

    Tue, Jun 6, 2023, 3:47 PM -03:00

--- prime.c
+++ prime.c
@@ -15,32 +15,41 @@ int main(void)
 
 void IsPrime(int initial_number, int final_number)
 {
-    if (initial_number == 1)
-    {
-        int j = 1;
-    }
-    else
-    {
-        int j = initial_number - 1;
-    }
+    // if (initial_number == 1)
+    //{
+    //     int j = 1;
+    // }
+    // else
+    //{
+    //     int j = initial_number - 1;
+    // }
 
     for (int i = initial_number; i < final_number; i++)
     {
+
         do
         {
-           int prime = prime + (i % j);
+            if (initial_number == 1)
+            {
+                int j = 1;
+            }
+            else
+            {
+                int j = initial_number - 1;
+            }
+            int prime = prime + (i % j);
             j++;
-        }
+        
 
         while (j < i);
 
-            if ((prime != 0))
-            {
-                printf("%i", i);
-            }
+        if ((prime != 0))
+        {
+            printf("%i", i);
         }
-
-        // int prime = (i % 2) + (i % 3) + (i % 5) + (i % 7) + (i % 9);
-        // if (prime =! 0 )
     }
+
+    // int prime = (i % 2) + (i % 3) + (i % 5) + (i % 7) + (i % 9);
+    // if (prime =! 0 )
+}
 }
