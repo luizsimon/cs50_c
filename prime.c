763fa0dd98851edaae97c5d53a2332430cab26d6
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-06 21:16:55 +0000

    Tue, Jun 6, 2023, 6:16 PM -03:00

--- prime.c
+++ prime.c
@@ -33,32 +33,13 @@ void IsPrime(int initial_number, int final_number)
     //     int j = initial_number - 1;
     // }
 
-    int soma = 0;
     for (int i = initial_number; i < final_number; i++)
     {
-        for (int j = 2; j < i; j++)
-        {
 
-            int prime = (i % j);
+        
 
-            if (prime == 0)
-            {
-                printf("\n%i - No Prime", i);
-                //return true;
-                break;
-            }
-            else
-            {
-                printf("\n%i - Prime", i);
-                //return false;
-                //break;
-            }
-        }
-        // printf("%i - é prime\n", i);
+
+
 
         printf("//");
     }
-    // int prime = (i % 2) + (i % 3) + (i % 5) + (i % 7) + (i % 9);
-    // if (prime =! 0 )
-    //return true;
-}
