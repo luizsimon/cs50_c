3d5aabaa13bcac4adfafc317a53d7da1f6750e0b
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-06 15:12:46 +0000

    Tue, Jun 6, 2023, 12:12 PM -03:00

--- credit.c
+++ credit.c
@@ -30,19 +30,17 @@ void validacao(long numero, int quantidade_digitos)
     int soma_demais_digitos = 0;
     int soma_digitos_multiplicados = 0;
     int soma_total = 0;
-
-    int digitos_cartao = 0;
     long x = 100;
-    long y = 10; // para pegar os outros numeros que foram descartados long int y = 10;
-    long e = pow(10, (quantidade_digitos - 2)); // funcao pow faz o exponente (base, expoente)
-    int dois_digitos_iniciais = (n - (n % e)) / e;
-    int primeiro_digito_inicial = (dois_digitos_iniciais - (dois_digitos_iniciais % 10)) / 10;
+    long y = 10;
+    long e = pow(10, (quantidade_digitos - 2)); // funcao pow faz a exponenciacao (base, expoente)
+    int dois_digitos_iniciais = (n - (n % e)) / e; // pega os 2 primeiros digitos
+    int primeiro_digito_inicial = (dois_digitos_iniciais - (dois_digitos_iniciais % 10)) / 10; // exclui o segundo digito, para validar os cartoes VISA
+
     while (i < 8) // é < 8 pq o máximo de um cartão é 16 digitos
     {
-        int digitos_multiplicados = (n % x) / (x / 10);
-        x = x * 100;
-        digitos_cartao = digitos_cartao + digitos_multiplicados;
-        int demais_digitos = (n % y) / (y / 10);
+        int digitos_multiplicados = (n % x) / (x / 10); //pega os digitos começando do penultimo pulando de 1 em 1
+        x = x * 100; // varia o x no calculo do digitos_multiplicados
+        int demais_digitos = (n % y) / (y / 10); //pega os outros digitos que nao foram pegos na digitos_multiplicados
         y = y * 100;
         i++;
         int soma = 2 * digitos_multiplicados;
