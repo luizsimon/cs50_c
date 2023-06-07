e66b909aa037cd4df10c3fde81864f3eed09526c
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-07 18:58:05 +0000

    Wed, Jun 7, 2023, 3:58 PM -03:00

--- prime.c
+++ prime.c
@@ -50,7 +50,8 @@ bool IsPrime(int initial_number, int final_number)
         prime = (i % j);
         j--;
     }
-    while ((j > 1 && prime != 0) || (i == j + 1 && i != 1)); // se tem &&, sendo uma delas falsa, todo o () é false. Se tem || pra ser falso todas precisam ser falso, senão é true
+    while ((j > 1 && prime != 0) || (i == j + 1 && i != 1)); // se tem &&, sendo uma delas falsa, todo o () é false.
+// Se tem || pra ser falso todas precisam ser falso, senão é true
 
     // vai retornar true se NAO FOR PRIMO e false SE FOR PRIMO
     if ((prime == 0 && i > 3))
