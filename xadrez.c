b54518dad5d9ba220813537b27d2b295cc88553f
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-07 22:55:47 +0000

    Wed, Jun 7, 2023, 7:55 PM -03:00

--- xadrez.c
+++ xadrez.c
@@ -31,7 +31,7 @@ void PrintXadrez(int tamanho)
             printf("*#");
             j++;
         }
-        while (j == tamanho/2);
+        while (j < tamanho/2);
 
         printf("\n");
     }
