6e09ae2f856e68569b788723ffe275ba69edea81
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-07 17:28:33 +0000

    Wed, Jun 7, 2023, 2:28 PM -03:00

--- prime.c
+++ prime.c
@@ -3,76 +3,45 @@
 #include <stdio.h>
 
 bool IsPrime(int initial_number, int final_number);
-//void PrintNumber(int initial_number, int final_number);
 
 int main(void)
 {
-    // IsPrime
-    // GetNumbers
-    int i = get_int("inicial: ");
-    int f = get_int("final: ");
-    // bool prime = IsPrime(i,f);
 
-    //IsPrime(i, f);
-    for (int m = i; m < f; m++)
+    int i = get_int("Minimum: ");
+    int f = get_int("Maximum: ");
+
+    for (int m = i; m <= f; m++)
     {
-        if (IsPrime(i, f))
+        if (m == 1)
         {
-            // PrintNumber(i,f);
-             printf("\n%i No Primo\n", m);
+
+        }
+        else if (IsPrime(m, f))
+        {
+
         }
         else
         {
-            //PrintNumber(i, f);
-            printf("\n%i Primo\n", m);
+            printf("%i\n", m);
         }
     }
-
-    // if (IsPrime(i, f))
-    //{
-    //      printf("- primo");
-    // }
-    //  else
-    //{
-    //      printf("- no primo");
-    //  }
 }
 
 bool IsPrime(int initial_number, int final_number)
 {
-    // if (initial_number == 1)
-    //{
-    //     int j = 1;
-    // }
-    // else
-    //{
-    //     int j = initial_number - 1;
-    // }
-
-    for (int i = initial_number; i < final_number; i++)
+    int i = initial_number;
+    int prime;
+    int j = i;
+    do
     {
-        int prime;
-        int j = i - 1;
-        do
-        {
-            prime = (i % j);
-            j--;
-            // printf("\n\nj = %i\n\n", j);
-        }
-        while ((j > 1 && prime != 0)); //|| (i == j + 1)
-        //printf("i = %i  prime = %i\n", i, prime);
-        if ((prime == 0 && i > 3))
-        {
-            return true;
-        }
+        prime = (i % j);
+        j--;
+    }
+    while ((j > 1 && prime != 0) || (i == j + 1 && i != 1));
 
+    if ((prime == 0 && i > 3))
+    {
+        return true;
     }
     return false;
 }
-// void PrintNumber(int initial_number, int final_number)
-//{
-//     for (int m = initial_number; m < final_number; m++)
-//     {
-//         printf("%i\n", m);
-//     }
-// }
