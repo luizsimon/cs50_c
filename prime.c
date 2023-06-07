3a80d7504473464451f64c875d2909f5808e75fd
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-07 14:49:26 +0000

    Wed, Jun 7, 2023, 11:49 AM -03:00

--- prime.c
+++ prime.c
@@ -50,7 +50,7 @@ void IsPrime(int initial_number, int final_number)
             prime = (i % j);
             j--;
         }
-        while (j == 2);
+        while (j == 2 || prime == 0);
 
         if (prime == 0 && i > 3)
         {
