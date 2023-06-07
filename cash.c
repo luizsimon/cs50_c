9f1f096e43c002d0f146f193881ab62ee7f9e992
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-07 20:14:20 +0000

    Wed, Jun 7, 2023, 5:14 PM -03:00

--- cash.c
+++ cash.c
@@ -3,8 +3,8 @@
 
 int get_cents(void);
 int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
+int calculate_dimes(int cents, int change_quarters);
+int calculate_nickels(int cents, int change_dimes);
 int calculate_pennies(int cents);
 
 int main(void)
@@ -17,22 +17,22 @@ int main(void)
     cents = cents - quarters * 25;
 
     // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+    //int dimes = calculate_dimes(cents);
+    //cents = cents - dimes * 10;
 
     // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+    //int nickels = calculate_nickels(cents);
+    //cents = cents - nickels * 5;
 
     // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
+    //int pennies = calculate_pennies(cents);
+    //cents = cents - pennies * 1;
 
     // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
+    //int coins = quarters + dimes + nickels + pennies;
 
     // Print total number of coins to give the customer
-    printf("%i\n", coins);
+    //printf("%i\n", coins);
 }
 
 int get_cents(void)
@@ -57,20 +57,32 @@ int calculate_quarters(int cents)
     return change_quarters;
 }
 
-int calculate_dimes(int cents)
+int calculate_dimes(int cents, int change_quarters)
 {
-    // TODO
-    return 0;
+    int dimes = cents - change_quarters*25;
+    int change_dimes;
+    int dimes_for = 0;
+    for (change_dimes = 0; change_dimes > 10; change_dimes++)
+    {
+        dimes_for = (dimes % 10) - dimes_for;
+    }
+    return change_dimes;
 }
 
-int calculate_nickels(int cents)
+int calculate_nickels(int cents, int change_dimes)
 {
-    // TODO
-    return 0;
+    int nickles = cents - change_dimes*10;
+    int change_nickles;
+    int nickles_for = 0;
+    for (change_nickles = 0; change_nickles > 10; change_nickles++)
+    {
+        nickles_for = (nickles % 10) - nickles_for;
+    }
+    return change_nickles;
 }
 
-int calculate_pennies(int cents)
+int calculate_pennies(int cents, int change_nickles)
 {
-    // TODO
+    int pennies = cents - 
     return 0;
 }
