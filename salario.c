d22f68df8acb21815dd138e2be2a3a0af3e19f2c
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-14 18:05:13 +0000

    Wed, Jun 14, 2023, 3:05 PM -03:00

--- salario.c
+++ salario.c
@@ -3,5 +3,21 @@
 
 int main(void)
 {
-    
+    float salario, salarioReajustado, salarioMinimo;
+    salario = get_float("Entre com o salario atual: ");
+    salarioMinimo = get_float("Salario Minimo: ");
+
+    if (salario <= salarioMinimo)
+    {
+        salarioReajustado = 1.25 * salario;
+    }
+    else if (salario <= 3 * salarioMinimo)
+    {
+        salarioReajustado = 1.5 * salario;
+    }
+    else
+    {
+        salarioReajustado = 1.1 * salario;
+    }
+    printf("Salario Reajustado igual a %.2f\n", salarioReajustado);
 }
