abecd5b4c4ec35754564d873533015e0e7c71146
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-06-14 17:41:02 +0000

    Wed, Jun 14, 2023, 2:41 PM -03:00

---
+++ media.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    float nota1, nota2, nota3, nota4;
+    string nome = get_string("Digite seu nome: \n");
+    nota1 = get_float("Nota 1: \n");
+    nota2 = get_float("Nota 2: \n");
+    nota3 = get_float("Nota 3: \n");
+    nota4 = get_float("Nota 4: \n");
+
+    float media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    if (media >= 7)
+    {
+        printf("%s, foi aprovado!\n", nome);
+    }
+    else
+    {
+        printf("%s, foi reprovado!\n", nome);
+    }
+}
