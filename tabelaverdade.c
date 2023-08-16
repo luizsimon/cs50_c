90c165391046cf81b96a1d5336755ce91d86baef
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-08-16 19:49:20 +0000

    Wed, Aug 16, 2023, 4:49 PM -03:00

--- tabelaverdade.c
+++ tabelaverdade.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int Entradas(void);
+
 int main(void)
 {
     printf("testezinho");
+    
 }
 
 int Entradas(void)
 {
-    int entradas = get_int("Qual o número de entradas?")
+    int entradas = get_int("Qual o número de entradas? \n");
+    return entradas;
 }
