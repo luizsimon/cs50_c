81ac6925352c100637951f10a306ab1b9b89f099
Luiz Simon <135388348+luizsimon@users.noreply.github.com>
2023-08-17 12:10:27 +0000

    Thu, Aug 17, 2023, 9:10 AM -03:00

--- tabelaverdade.c
+++ tabelaverdade.c
@@ -9,7 +9,7 @@ int main(void)
     int entradas = get_int("Qual o número de entradas? ");
     int linhas = pow(2, entradas);
     TabelaVerdade(entradas, linhas);
-    
+
     return 0;
 }
 
@@ -19,15 +19,15 @@ void TabelaVerdade(int entradas, int linhas)
     printf ("A B C D E F");
     printf ("\n");
     int j = linhas;
+    int i = entradas;
+    
     while (j > 0)                       // aqui o i = a posicao do numero e o j = a linha
     {
-        int i = entradas;
-
         while (i > 0)
         {
             int x = pow(2, i);
             int y = pow(2, i-1);
-            // no if, verica se a linha (j) divida por 2^i (posição) é 0 ou é maior que 2^(i-1)
+            // no if, verica se a linha (j) dividida por 2^i (posição) é 0 ou é maior que 2^(i-1)
             if ((j % x) == 0 || (j % x) > y)
             {
                 printf("0 ");
